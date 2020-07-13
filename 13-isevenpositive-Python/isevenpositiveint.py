e81ec8905f7f77bf089689a59f6c9642900a98cb
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-13 19:22:47 +0530

    Jul 13  19:22:46

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -3,7 +3,10 @@
 # integer, and it is positive, and it is even (all 3 must be True), or False otherwise. Do not
 # crashing if the value is not an integer. So, isevenpositiveint("yikes!") returns False (rather
 # than crashing), and isevenpositiveint(123456) returns True.
+#author: Prasanna Saripudi
 
 def isevenpositiveint(x):
-	# your code goes here
-	pass
+	# checking type, positive, even
+	if type(x) is int and x >0 and x%2==0:
+		return True
+	return False
