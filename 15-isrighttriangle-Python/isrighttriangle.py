3e2bd922913b712e1ce4461bc221f940420c7af0
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-13 20:12:32 +0530

    cases passed

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -4,7 +4,24 @@
 # a right triangle and False otherwise. You may wish to write a helper function,
 # distance(x1, y1, x2, y2), which you might call several times. Also, remember to use
 # almostEqual (instead of ==) when comparing floats.
+# author: Prasanna Saripudi
+import math
+from math import isclose
+
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
-	# your code goes here
-	pass
+    # calculate the sides of traingle
+    a = distance(x1, y1, x2, y2)
+    b = distance(x2, y2, x3, y3)
+    c = distance(x1, y1, x3, y3)
+    sides = [a, b, c]
+    maxSideInt = sides.index(max(sides))
+    remSides = [i for i in [0, 1, 2] if i != maxSideInt]
+    # check if hyp sqr = sum of sqr of two rem
+    if isclose(sides[remSides[0]]**2 + sides[remSides[1]]**2, sides[maxSideInt]**2):
+        return True
+    return False
+
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt(((x2-x1)**2)+((y2-y1)**2))
