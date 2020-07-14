9c133fb88b1cb84aa66dcd363af3ee8ce4b6abef
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-14 11:28:11 +0530

    Jul 14  11:28:10

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -2,6 +2,19 @@
 Fibonacci sequence value.
 Your code should have the same input/output as the 
 iterative code in the instructions."""
+# author: Prasanna Saripudi
+# 0 1 2 3 5 8 13 21 34
+
 
 def get_fib(position):
-    return -1
+    if position in [0, 1]:
+        return 0
+    elif position == 2:
+        return 1
+    else:
+        x = get_fib(position - 1)
+        y = get_fib(position-2)
+        s = x + y
+        print(x, y)
+        print(position - 1, position-2, s)
+        return s
