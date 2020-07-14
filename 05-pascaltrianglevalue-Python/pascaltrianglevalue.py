37c8feb2ec7159f53fd7cb7126813ecc27a64f68
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-14 12:34:57 +0530

    Jul 14  12:34:57

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -1,12 +1,23 @@
-# Write the function pascalsTriangleValue(row, col) 
-# that takes int values row and col, and returns the 
-# value in the given row and column of Pascal's 
-# Triangle where the triangle starts at row 0, and 
-# each row starts at column 0. If either row or col 
-# are not legal values, return None, instead of crashing. 
+# Write the function pascalsTriangleValue(row, col)
+# that takes int values row and col, and returns the
+# value in the given row and column of Pascal's
+# Triangle where the triangle starts at row 0, and
+# each row starts at column 0. If either row or col
+# are not legal values, return None, instead of crashing.
+# author: Prasanna Saripudi
 
 
-
-
+# formula of the coeff value [n!/(k!*(n-k)!)] n, k are row, culumns in pascal
 def fun_pascaltrianglevalue(row, col):
-	return 1
+    if col > row:
+        return 0
+    elif row == col or col == 0:
+        return 1
+    coeff = fact(row) // (fact(col)*fact(row-col))
+    return coeff
+
+
+def fact(num):
+    if num == 1:
+        return 1
+    return num*fact(num-1)
