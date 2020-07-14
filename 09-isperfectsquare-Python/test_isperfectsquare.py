a5260602c4d7ec633a4dc4d43aa79712cacd3dbd
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-14 15:31:46 +0530

    Jul 14  15:31:45

--- 09-isperfectsquare-Python/test_isperfectsquare.py
+++ 09-isperfectsquare-Python/test_isperfectsquare.py
@@ -1,18 +1,17 @@
-import pytest
-from isperfectsquare import isperfectsquare
-import os
-import sys
+import os,sys
 sys.path.append(os.getcwd())
+from isperfectsquare import isperfectsquare
+import pytest
 
 
-@pytest.mark.parametrize('n, result', [
-    ("hello", False),
-    (6.25, False),
-    (625, True),
-    (-625, False),
-    (4, True),
-    (100, True),
-    ("100", True),
+@pytest.mark.parametrize('n, result',[
+	("hello", False), 
+	(6.25, False), 
+	(625, True), 
+	(-625, False), 
+	(4, True), 
+	(100, True), 
+	("100", True), 
 ])
-def test_isperfectsquare(n, result):
-    assert isperfectsquare(n) == result
+def test_isperfectsquare(row, result):
+    assert isperfectsquare(row) == result
