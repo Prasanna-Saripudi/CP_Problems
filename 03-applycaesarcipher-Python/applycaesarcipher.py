379977edfed56c68783df4bad1b94f2330d4a92f
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-15 21:18:31 +0530

    Jul 15  21:18:30

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -1,18 +1,27 @@
-# A Caesar Cipher is a simple cipher that works by shifting each letter in 
-# the given message by a certain number. For example, if we shift the message 
+# A Caesar Cipher is a simple cipher that works by shifting each letter in
+# the given message by a certain number. For example, if we shift the message
 # "We Attack At Dawn" by 1 letter, it becomes "Xf Buubdl Bu Ebxo"
-# Write the function applyCaesarCipher(message, shift) which shifts the given 
-# message by shift letters. You are guaranteed that message is a string, and 
-# that shift is an integer between -25 and 25. Capital letters should stay capital 
-# and lowercase letters should stay lowercase, and non-letter characters should not be changed. 
+# Write the function applyCaesarCipher(message, shift) which shifts the given
+# message by shift letters. You are guaranteed that message is a string, and
+# that shift is an integer between -25 and 25. Capital letters should stay capital
+# and lowercase letters should stay lowercase, and non-letter characters should not be changed.
 # Note that "Z" wraps around to "A". So, for example:
 # assert(applyCaesarCipher("We Attack At Dawn", 1) == "Xf Buubdl Bu Ebxo")
 # assert(applyCaesarCipher("zodiac", -2) == "xmbgya")
-
+# author: Prasanna Saripudi
 
 def fun_applycaesarcipher(msg, shift):
-	return ""
-
-
-
-
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    result = []
+    for x in msg:
+        if x.isalpha():
+            if x.islower():
+                c = msg[msg.index(x) + shift]
+                result.append(c)
+            elif x.isupper():
+                c = msg[msg.index(x.lower()) + shift].upper()
+                result.append(c)
+        else:
+            result.append(x)
+        print("".join(result))
+    return "".join(result)
