ea437df0e954c65d7c4197b513b23f4ee9065f37
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-16 16:29:58 +0530

    passed

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -9,7 +9,18 @@ meaning there are no repeated values, and
 elements are in a strictly increasing order.
 Return the index of value, or -1 if the value
 doesn't exist in the list."""
+# author: Prasanna Saripudi
+
 
 def binary_search(input_array, value):
-    # Your code goes here
-    pass
+    low = 0
+    high = len(input_array) - 1
+    while low <= high:
+        mid = (low+high)//2
+        if value > input_array[mid]:
+            low = mid + 1
+        elif value < input_array[mid]:
+            high = mid - 1
+        else:
+            return mid
+    return -1
