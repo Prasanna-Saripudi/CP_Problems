5e12d5b738710ccc2315c0a6fd1dad4e1cc912e2
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-16 10:51:39 +0530

    Jul 16  10:51:39

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -7,7 +7,7 @@
 
 def fun_replace(s1, s2, s3):
     for i in range(len(s1) - len(s2)):
+        print(s1[i: i + len(s2)], s2)
         if s1[i: i + len(s2)] == s2:
-            print(s1[i: i + len(s2)], s2)
             return s1[:i]+s3+s1[i+len(s2):]
     return s1
