d71dfcaf7d2dcb146a92a9c143a0884e2fed5398
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-16 11:04:18 +0530

    Jul 16  11:04:18

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -6,8 +6,20 @@
 
 
 def fun_replace(s1, s2, s3):
-    for i in range(len(s1) - len(s2)):
-        print(s1[i: i + len(s2)], s2)
-        if s1[i: i + len(s2)] == s2:
-            return s1[:i]+s3+s1[i+len(s2):]
+    freq = s1.count(s2)
+    if freq == 0:
+        return s1
+    elif freq == 1:
+        index = s1.index(s2)
+        return s1[:index] + s3 + s1[index + len(s2):]
+    indexes = [i for i in range(len(s1) - len(s2) + 1) if s1.startswith(s2, i)]
+    j = indexes[0]
+    for i in indexes[1:]:
+        s1 = s1[:j] + s3 + s1[j + len(s2):]
+        j += i
     return s1
+# for i in range(len(s1) - len(s2)+1):
+#     print(s1[i: i + len(s2)], s2)
+#     if s1[i: i + len(s2)] == s2:
+#         return s1[:i]+s3+s1[i+len(s2):]
+# return s1
