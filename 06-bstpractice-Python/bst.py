8c1ff84f0bd2db0dc12c378b06a83265d3079abe
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-17 12:38:24 +0530

    pass

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -15,33 +15,48 @@ class BST(object):
         self.insert1(self.root, new_val)
 
     def insert1(self, root, new_val):
+        # recursive helper function for inserting
         if root:
             if root.value > new_val:
-                if root.left is None:
+                if root.left is not None:
                     self.insert1(root.left, new_val)
                 else:
-                    root.left = new_val
-            if root.value < new_val:
-                if root.right is None:
+                    root.left = Node(new_val)
+            elif root.value < new_val:
+                if root.right is not None:
                     self.insert1(root.right, new_val)
                 else:
-                    root.right = new_val
+                    root.right = Node(new_val)
         else:
-            root = new_val
+            root = Node(new_val)
 
     def printSelf(self):
-        # Your code goes here
-        pass
+        self.printSelf1(self.root)
+
+    def printSelf1(self, root):
+        # recursive iterative dunction for print
+        if root is None:
+            return
+        print(" ", root.value)
+        self.printSelf1(root.left)
+        self.printSelf1(root.right)
 
     def search(self, find_val):
-        if self.root:
+        if self.root and isinstance(find_val, int):
             root = self.search1(self.root, find_val)
+            f = True if root else False
+            print(f)
             return True if root else False
         return False
 
     def search1(self, root, find_val):
+        # recursive iterative function for searching
         if root is None or root.value == find_val:
+            print("in middle")
             return root
         if root.value > find_val:
+            print("in left")
             return self.search1(root.left, find_val)
-        return self.search1(root.right, find_val)
+        if root.value < find_val:
+            print("in right")
+            return self.search1(root.right, find_val)
