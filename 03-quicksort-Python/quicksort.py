4e3d2270bdbc4466fa6821c00dd1aa9354b2431d
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-17 11:47:07 +0530

    passed

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -8,15 +8,17 @@ def quicksort(array):
     quicksort1(array, 0, len(array)-1)
     return array
 
+
 def quicksort1(array, low, high):
-	# recursive function continues until sorted
+    # recursive function continues until sorted
     if low < high:
         parted = partition(array, low, high)
         quicksort1(array, low, parted - 1)
         quicksort1(array, parted+1, high)
 
+
 def partition(array, low, high):
-	# parts the array based on pivot and returns the parted value
+    # parts the array based on pivot and returns the parted value
     # taking first elemnt as pivot
     pivot = array[low]
     start = low + 1
--- 03-quicksort-Python/test_solution.py
+++ 03-quicksort-Python/test_solution.py
@@ -1,10 +1,11 @@
 
-import os,sys
-sys.path.append(os.getcwd())
-from quicksort import quicksort
 import pytest
+from quicksort import quicksort
+import os
+import sys
+sys.path.append(os.getcwd())
 
-@pytest.mark.parametrize("input_value, result",[([21, 4, 1, 3, 9, 20, 25, 6, 21, 14],[1, 3, 4, 6, 9, 14, 20, 21, 21, 25])])
-def test_quicksort(input_value, result):    
+
+@pytest.mark.parametrize("input_value, result", [([21, 4, 1, 3, 9, 20, 25, 6, 21, 14], [1, 3, 4, 6, 9, 14, 20, 21, 21, 25])])
+def test_quicksort(input_value, result):
     assert quicksort(input_value) == result
-
