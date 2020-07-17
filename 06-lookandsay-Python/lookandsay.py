a9e3ca60dab7fda1fb7bffe5fc5433ecdde2e06e
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-17 18:38:52 +0530

    Jul 17  18:38:51

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -1,12 +1,24 @@
-# First, you can read about look-and-say numbers here. Then, write the function lookAndSay(a) that takes a list of 
-# numbers and returns a list of numbers that results from "reading off" the initial list using the look-and-say 
+# First, you can read about look-and-say numbers here. Then, write the function lookAndSay(a) that takes a list of
+# numbers and returns a list of numbers that results from "reading off" the initial list using the look-and-say
 # method, using tuples for each (count, value) pair. For example:
 # lookAndSay([]) == []
 # lookAndSay([1,1,1]) == [(3,1)]
 # lookAndSay([-1,2,7]) == [(1,-1),(1,2),(1,7)]
 # lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
+# author: Prasanna Saripudi
+from collections import OrderedDict
+
 
 def lookandsay(a):
-	# Your code goes here
-	pass
+    # can consider using ordered sets for having the sets in an order
+    # from ordered_set import OrderedSet;os = OrderedSet(list)
+    dict = OrderedDict()
+    final = []
+    for val in a:
+        if val not in dict.keys():
+            c = a.count(val)
+            dict[val] = a.count(val)
+    for key, value in dict.items():
+        final.append((value, key))
+    return final
