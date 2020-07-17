e441e103857d9741801d1931c0cda2ebccb0fa19
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-17 15:18:59 +0530

    .

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -4,6 +4,7 @@ class Node(object):
         self.left = None
         self.right = None
 
+
 class BinaryTree(object):
     def __init__(self, root):
         self.root = Node(root)
@@ -12,24 +13,37 @@ class BinaryTree(object):
         """Return True if the value
         is in the tree, return
         False otherwise."""
-        # Your code goes here
-        pass
+        if self.root and isinstance(find_val, int):
+            bool = self.preorder_search(self.root, find_val)
+            return bool
+        else:
+            return False
 
     def print_tree(self):
         """Print out all tree nodes
         as they are visited in
         a pre-order traversal."""
         # Your code goes here
-        pass
+        self.preorder_print(self.root)
 
     def preorder_search(self, start, find_val):
         """Helper method - use this to create a 
         recursive search solution."""
-        # Your code goes here
-        pass
+        if start == None:
+            return False
+        if start.value == find_val:
+            return True
+        leftcheck = self.preorder_search(start.left, find_val)
+        if leftcheck:
+            return True
+        rightcheck = self.preorder_search(start.right, find_val)
+        return rightcheck
 
-    def preorder_print(self, start, traversal):
+    def preorder_print(self, start):
         """Helper method - use this to create a 
-        recursive print solution."""
-        # Your code goes here
-        pass
+        recursive print solution. """
+        if start:
+            print(str(start.value) + " ")
+            self.preorder_print(start.left)
+            self.preorder_print(start.right)
+        
