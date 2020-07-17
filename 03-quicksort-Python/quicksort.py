e846706f4e6de791f824fb3f9a7ef9ef7ec5f06c
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-17 11:41:39 +0530

    Jul 17  11:41:39

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -1,6 +1,38 @@
 """Implement quick sort in Python.
 Input a list.
 Output a sorted list."""
+# author: Prasanna Saripudi
+
+
 def quicksort(array):
-	# Your code goes here
-	pass
+    quicksort(array, 0, len(array))
+    return array
+
+# recursive function continues until sorted
+
+
+def quicksort(array, low, high):
+    if low < high:
+        parted = partition(array, low, high)
+        quicksort(array, low, parted - 1)
+        quicksort(array, parted+1, high)
+
+# parts the array based on pivot and returns the parted value
+
+
+def partition(array, low, high):
+    # taking first elemnt as pivot
+    pivot = array[low]
+    start = low + 1
+    end = high
+    while True:
+        while start <= end and array[start] <= pivot:
+            start += 1
+        while start <= end and array[end] >= pivot:
+            end -= 1
+        if start <= end:
+            array[start], array[end] = array[end], array[start]
+        else:
+            break
+    array[low], array[end] = array[end], array[start]
+    return end
