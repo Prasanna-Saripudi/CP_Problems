a3d03ab92d5125e5fe30debed7a6d354ab1fbd9d
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-20 11:09:00 +0530

    passed

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -1,7 +1,19 @@
-# Write the function hasnoprimes(L) that takes a 2d list L of integers, 
+# Write the function hasnoprimes(L) that takes a 2d list L of integers,
 # and returns True if L does not contain any primes, and False otherwise.
-
+# author: Prasanna Saripudi
 
 def fun_hasnoprimes(l):
-	return True
+    for listi in l:
+        for ele in listi:
+            if isPrime(ele):
+                return False
+    return True
 
+
+def isPrime(num):
+    if num <= 1:
+        return False
+    for i in range(2, ((num // 2) + 1)):
+        if num % i == 0:
+            return False
+    return True
