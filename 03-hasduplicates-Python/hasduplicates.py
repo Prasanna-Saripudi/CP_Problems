983b4ea26675ec26fb9444084ff0a6d7b1d6afd0
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-20 11:28:28 +0530

    Jul 20  11:28:28

--- 03-hasduplicates-Python/hasduplicates.py
+++ 03-hasduplicates-Python/hasduplicates.py
@@ -1,8 +1,13 @@
 # hasDuplicates(L) [15 pts]
-# Write the function hasDuplicates(L) that takes a 2d list L of arbitrary values, and returns True if L 
-# contains any duplicate values (that is, 
+# Write the function hasDuplicates(L) that takes a 2d list L of arbitrary values, and returns True if L
+# contains any duplicate values (that is,
 # if any two values in L are equal to each other), and False otherwise.
+# author: Prasanna Saripudi
 
 def hasduplicates(L):
-	# Your code goes here
-	pass
+    # for flattening 2d to 1d list
+    listi = sum(L, [])
+    # checking the 1d list length and the set length for duplicates
+    if len(listi) > len(set(listi)):
+        return True
+    return False
