88a91e893085586d5e6bdcf40556d1be5c94e8ba
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-21 16:37:21 +0530

    Jul 21  16:37:20

--- 02-carrylessadd-Python/carrylessadd.py
+++ 02-carrylessadd-Python/carrylessadd.py
@@ -1,10 +1,17 @@
-# carry less addition means a  normal addition with the carry from each column ignored. 
-# So, for example, if we carryless-ly add 8+7, we get 5 (ignore the carry). And if we add 
-# 18+27, we get 35 (still ignore the carry). With this in mind, write the function 
-# fun_carrylessadd(x, y) that takes two non-negative integers x and y and returns their 
+# carry less addition means a  normal addition with the carry from each column ignored.
+# So, for example, if we carryless-ly add 8+7, we get 5 (ignore the carry). And if we add
+# 18+27, we get 35 (still ignore the carry). With this in mind, write the function
+# fun_carrylessadd(x, y) that takes two non-negative integers x and y and returns their
 # carryless sum. As the paper demonstrates, fun_carrylessadd(785, 376) returns 51.
-
+# author: Prasanna Saripudi
 
 def fun_carrylessadd(x, y):
-	return 0
-
+    xStr, yStr = str(x)[::-1], str(y)[::-1]
+    minStr, maxStr = min(xStr, yStr), max(xStr, yStr)
+    result = ''
+    for i in range(len(minStr)):
+        result += str(int(minStr[i]) + int(maxStr[i]))[-1]
+        print(i, result,)
+        if i < len(maxStr):
+            result += maxStr[i+1:]
+    return int(result[::-1])
