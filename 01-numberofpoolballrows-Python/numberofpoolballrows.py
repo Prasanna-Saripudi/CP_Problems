930487cce3c213788a4fed97daf11dfb783f4ddc
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-21 16:20:37 +0530

    passed

--- 01-numberofpoolballrows-Python/numberofpoolballrows.py
+++ 01-numberofpoolballrows-Python/numberofpoolballrows.py
@@ -1,10 +1,19 @@
 # Pool balls are arranged in balls where the first row contains 1 pool ball and each row contains 1
-# more pool ball than the previous row. 
-# numberOfPoolBallRows(balls) that takes a non-negative int number of pool balls, and returns the 
-# smallest int number of balls required for the given number of pool balls. Thus, numberOfPoolBallRows(6) 
-# returns 3. Note that if any balls must be in a row, then you count that row, and so 
+# more pool ball than the previous row.
+# numberOfPoolBallRows(balls) that takes a non-negative int number of pool balls, and returns the
+# smallest int number of balls required for the given number of pool balls. Thus, numberOfPoolBallRows(6)
+# returns 3. Note that if any balls must be in a row, then you count that row, and so
 # numberOfPoolBallRows(7) returns 4 (since the 4th row must have a single ball in it).
-
+# author: Prasanna Saripudi
 def fun_numberofpoolballrows(balls):
-	return 0
-
+    if balls == 1:
+        return 1
+    count, val, sum = 1, 1, 1
+    while True:
+        count += 1
+        if sum + val + 1 < balls:
+            val += 1
+            sum += val
+        else:
+            break
+    return count
