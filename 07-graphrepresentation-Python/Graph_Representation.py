12eda4e1ec7bb64dc8e955eb9584b693bc9ca7d9
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-21 12:48:39 +0530

    .

--- 07-graphrepresentation-Python/Graph_Representation.py
+++ 07-graphrepresentation-Python/Graph_Representation.py
@@ -1,13 +1,17 @@
+# author: Prasanna Saripudi
+
 class Node(object):
     def __init__(self, value):
         self.value = value
         self.edges = []
+
 
 class Edge(object):
     def __init__(self, value, node_from, node_to):
         self.value = value
         self.node_from = node_from
         self.node_to = node_to
+
 
 class Graph(object):
     def __init__(self, nodes=[], edges=[]):
@@ -17,7 +21,7 @@ class Graph(object):
     def insert_node(self, new_node_val):
         new_node = Node(new_node_val)
         self.nodes.append(new_node)
-        
+
     def insert_edge(self, new_edge_val, node_from_val, node_to_val):
         from_found = None
         to_found = None
@@ -44,8 +48,8 @@ class Graph(object):
     def get_adjacency_list(self):
         adjacency_list = [None] * (max_index + 1)
         return adjacency_list
-    
-    
+
     def get_adjacency_matrix(self):
-        adjacency_matrix = [[0 for i in range(max_index + 1)] for j in range(max_index + 1)]
+        adjacency_matrix = [
+            [0 for i in range(max_index + 1)] for j in range(max_index + 1)]
         return adjacency_matrix
--- 07-graphrepresentation-Python/test_solution.py
+++ 07-graphrepresentation-Python/test_solution.py
@@ -1,8 +1,9 @@
 
-import os,sys
-sys.path.append(os.getcwd())
-from Graph_Representation import Graph 
+from Graph_Representation import Graph
 import pytest
+import os
+import sys
+sys.path.append(os.getcwd())
 
 graph = Graph()
 graph.insert_edge(100, 1, 2)
@@ -11,15 +12,16 @@ graph.insert_edge(102, 1, 4)
 graph.insert_edge(103, 3, 4)
 
 
-@pytest.mark.parametrize("result",[([(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)])])
-def test_get_edge_list(result):    
+@pytest.mark.parametrize("result", [([(100, 1, 2), (101, 1, 3), (102, 1, 4), (103, 3, 4)])])
+def test_get_edge_list(result):
     assert graph.get_edge_list() == result
 
-@pytest.mark.parametrize("result",[([None, [(2, 100), (3, 101), (4, 102)], None, [(4, 103)], None])])
-def test_get_adjacency_list(result):    
+
+@pytest.mark.parametrize("result", [([None, [(2, 100), (3, 101), (4, 102)], None, [(4, 103)], None])])
+def test_get_adjacency_list(result):
     assert graph.get_adjacency_list() == result
 
-@pytest.mark.parametrize("result",[([[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]])])
-def test_get_adjacency_matrix(result):    
-    assert graph.get_adjacency_matrix() == result
 
+@pytest.mark.parametrize("result", [([[0, 0, 0, 0, 0], [0, 0, 100, 101, 102], [0, 0, 0, 0, 0], [0, 0, 0, 0, 103], [0, 0, 0, 0, 0]])])
+def test_get_adjacency_matrix(result):
+    assert graph.get_adjacency_matrix() == result
