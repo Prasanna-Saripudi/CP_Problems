0f4f345a1d2180203b2e1c66615f9aa6b3d939a6
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-21 23:42:08 +0530

    passed

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -1,9 +1,17 @@
 # nthWithProperty309(n) [20 pts]
-# We will say that a number n has "Property309" if its 5th power contains every 
-# digit (from 0 to 9) at least once. 309 is the smallest number with this property. 
-# Write the function nthWithProperty309 that takes a non-negative int n and returns 
+# We will say that a number n has "Property309" if its 5th power contains every
+# digit (from 0 to 9) at least once. 309 is the smallest number with this property.
+# Write the function nthWithProperty309 that takes a non-negative int n and returns
 # the nth number with Property309.
+# author: Prasanna Saripudi
 
 def nthwithproperty309(n):
-	# Your code goes here
-	pass
+    count, value = 0, 309
+    checkList = list("0123456789")
+    while count < n:
+        value += 1
+        powOf5 = value ** 5
+        list1 = list(str(powOf5))
+        if set(checkList).issubset(set(list1)):
+            count += 1
+    return value
