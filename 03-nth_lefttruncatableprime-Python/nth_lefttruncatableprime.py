b7a3e16c5edb317b2ac6f2f8966a234a99be2f67
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-22 17:58:08 +0530

    Jul 22  17:58:08

--- 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
+++ 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
@@ -1,14 +1,37 @@
 # Write the function nthLeftTruncatablePrime(n).
-# A Left-truncatable prime is a prime which in a given base (say 10) does not contain 0 
-# and which remains prime when the leading (left) digit is successively removed. 
-# For example, 317 is left-truncatable prime since 317, 17 and 7 are all prime. 
+# A Left-truncatable prime is a prime which in a given base (say 10) does not contain 0
+# and which remains prime when the leading (left) digit is successively removed.
+# For example, 317 is left-truncatable prime since 317, 17 and 7 are all prime.
 # There are total 4260 left-truncatable primes.
-# So nthLeftTruncatablePrime(0) retunearestKaprekarNumber(n)rns 2, and 
+# So nthLeftTruncatablePrime(0) retunearestKaprekarNumber(n)rns 2, and
 # nthLeftTruncatablePrime(10) returns 53.
-
-
-
-import math
+# author: Prasanna Saripudi
 
 def fun_nth_lefttruncatableprime(n):
-    return 1
+    count, value = 0, 2
+    while count < n:
+        value += 1
+        if isLeftTruncPrime(value):
+            count += 1
+    return value
+
+
+def isLeftTruncPrime(num):
+    numStr = str(num)[::-1]
+    if '0' in numStr:
+        return False
+    for i in range(len(numStr)):
+        x = int(numStr[:i+1][::-1])
+        print(x)
+        if not isPrime(x):
+            return False
+    return True
+
+
+def isPrime(num):
+    if num <= 1:
+        return False
+    for i in range(2, ((num // 2) + 1)):
+        if num % i == 0:
+            return False
+    return True
