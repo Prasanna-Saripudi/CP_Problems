3783b63b5a3797a524a18ca14774d3e06d8b34b2
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-22 23:50:42 +0530

    Jul 22  23:50:42

--- 03-nth_pronicnumber-Python/nthpronicnumber.py
+++ 03-nth_pronicnumber-Python/nthpronicnumber.py
@@ -3,6 +3,23 @@
 # Number. Pronic number is a number which is the product of two consecutive integers, that is, a
 # number n is a product of x and (x+1).
 # author: Prasanna Saripudi
+import math
+
 
 def nthpronicnumber(n):
-    return n*(n+1)
+    # nth number would be n*(n+1)
+    # return n*(n+1)
+    # otherwise need to check and iterate to see if a number is pronic or not
+    count, value = 0, 0
+    while count < n:
+        value += 1
+        if isPronic(value):
+            count += 1
+    return value
+
+
+def isPronic(num):
+    for i in range(int(math.sqrt(num))+1):
+        if i * (i + 1) == num:
+            return True
+    return False
