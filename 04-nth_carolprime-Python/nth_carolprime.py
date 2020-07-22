d3a4599174e07117f883c7183a094e7c5f5db60b
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-22 18:49:34 +0530

    Jul 22  18:49:34

--- 04-nth_carolprime-Python/nth_carolprime.py
+++ 04-nth_carolprime-Python/nth_carolprime.py
@@ -1,13 +1,28 @@
-# Write the function nthCarolPrime(n), which takes a non-negative int and returns the nth Carol Prime, 
-# which is a prime number of the form ((2**k - 1)**2 - 2) for some value positive int k. For example, 
-# if k equals 3, ((2**3 - 1)**2 -2) equals 47, which is prime, and so 47 is a Carol Prime. 
-# The first several Carol primes are: 7, 47, 223, 959, 3967, 16127, 65023, 261119, 1046527,... As such, 
+# Write the function nthCarolPrime(n), which takes a non-negative int and returns the nth Carol Prime,
+# which is a prime number of the form ((2**k - 1)**2 - 2) for some value positive int k. For example,
+# if k equals 3, ((2**3 - 1)**2 -2) equals 47, which is prime, and so 47 is a Carol Prime.
+# The first several Carol primes are: 7, 47, 223, 959, 3967, 16127, 65023, 261119, 1046527,... As such,
 # nthCarolPrime(0) returns 7.
-# Note: You must use a reasonably efficient approach that quickly works up to n==9, which 
-# will return a 12-digit answer! In particular, this means you cannot just edit isPrime. 
-# Hint: you may need to generate only Carol numbers, and then test those as you go 
+# Note: You must use a reasonably efficient approach that quickly works up to n==9, which
+# will return a 12-digit answer! In particular, this means you cannot just edit isPrime.
+# Hint: you may need to generate only Carol numbers, and then test those as you go
 # for primality (and you may need to think about that hint for a while for it to make sense!).
-
+# author: Prasanna Saripudi
 
 def fun_nth_carolprime(n):
-    return 0
+    count, value = 0, 3
+    while count < n:
+        x = ((2 ** value - 1) ** 2 - 2)
+        if isPrime(x):
+            count += 1
+        value += 1
+    return ((2 ** (value-1) - 1) ** 2 - 2)
+
+
+def isPrime(num):
+    if num <= 1:
+        return False
+    for i in range(2, ((num // 2) + 1)):
+        if num % i == 0:
+            return False
+    return True
