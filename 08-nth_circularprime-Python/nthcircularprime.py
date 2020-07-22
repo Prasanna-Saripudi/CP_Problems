2e2c7d5a94b55ce435f4b7c679daf911994c98f4
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-22 21:13:25 +0530

    Jul 22  21:13:25

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -5,13 +5,15 @@
 # 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197... To see why 197 is a Circular prime,
 # note that 197 is prime, as is 971 (rotated left), as is 719 (rotated left again).
 # author: Prasanna Saripudi
+import math
+
 
 def nthcircularprime(n):
     count, value = 0, 2
     while count < n:
         value += 1
         if isCircularPrime(value):
-            print(value)
+            # print(value)
             count += 1
     return value
 
@@ -31,9 +33,11 @@ def isCircularPrime(num):
 
 
 def isPrime(num):
-    if num <= 1:
+    if num <= 1 or num % 2 == 0 or num % 3 == 0:
         return False
-    for i in range(2, ((num // 2) + 1)):
+    elif num == 2 or num == 3:
+        return True
+    for i in range(5, int(math.sqrt(num))+1, 6):
         if num % i == 0:
             return False
     return True
