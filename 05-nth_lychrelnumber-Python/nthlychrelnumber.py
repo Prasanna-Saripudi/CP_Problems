39f3091d2e39b6c912c84ddf3e3ab2d455ccfd94
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-23 00:55:18 +0530

    Jul 23  00:55:18

--- 05-nth_lychrelnumber-Python/nthlychrelnumber.py
+++ 05-nth_lychrelnumber-Python/nthlychrelnumber.py
@@ -1,10 +1,22 @@
 # nthLychrelNumber(n) [20 pts]
-# A Lychrel number is a natural number that cannot form a palindrome through the iterative process of 
-# repeatedly reversing its digits and adding the resulting numbers. This process is sometimes called the 
+# A Lychrel number is a natural number that cannot form a palindrome through the iterative process of
+# repeatedly reversing its digits and adding the resulting numbers. This process is sometimes called the
 # 196-algorithm, after the most famous number associated with the process.
 # The first few Lychrel numbers are 196, 295, 394, 493, 592, 689, 691, 788, 790, 879, 887….
-
+# author: Prasanna Saripudi
 
 def nthlychrelnumbers(n):
-	# your code goes here
-	pass
+    count, value = 1, 196
+    while count < n:
+        value += 1
+        if isLychrel(value):
+            count += 1
+    return value
+
+
+def isLychrel(num):
+    for i in range(10):
+        num = num + int(str(num)[::-1])
+        if num == int(str(num)[::-1]):
+            return False
+    return True
