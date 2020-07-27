bb0d44c3610d1b1770a93745825b19167f0e19fc
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-27 11:20:07 +0530

    passed

--- 04-circlesintersect-Java/circlesintersect.java
+++ 04-circlesintersect-Java/circlesintersect.java
@@ -3,9 +3,15 @@
 // # that describe the circle centered at (x1,y1) with radius r1, 
 // # and the circle centered at (x2,y2) with radius r2, and returns True 
 // # if the two circles intersect and False otherwise.
-
+import java.lang.Math;
 class circlesintersect {
 	public boolean fun_circlesintersect(int x1, int y1, int r1, int x2, int y2, int r2){
-		return false;
+		int dist = (int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		if(r1+r2 < dist) return false;
+		return true;
 	}
+	public static void main(String[] args) {
+		
+	}
+
 }
