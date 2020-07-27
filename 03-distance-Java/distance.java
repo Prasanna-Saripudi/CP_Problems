d6c790843066e06f52f24d945cb177a1802eaea6
Prasanna-Saripudi <55692002+Prasanna-Saripudi@users.noreply.github.com>
2020-07-27 11:06:35 +0530

    passed

--- 03-distance-Java/distance.java
+++ 03-distance-Java/distance.java
@@ -2,10 +2,15 @@
 // # that takes four int values x1, y1, x2, y2 
 // # that represent the two points (x1, y1) and (x2, y2), 
 // # and returns the distance between those points as a int.
+import java.lang.Math;
 
 class distance {
+
 	public int fun_distance(int x1, int y1, int x2, int y2){
-		// Your code goes here
-		return 0;
+		return (int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	}
+	public static void main(String[] args) {
+		
+		
 	}
 }
